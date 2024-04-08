1848d006a4a132119c8fe4abe2a38ac26c01ab31
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-08 22:50:40 +0330

    Adding sidebar component to root layout.tsx

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,11 +1,12 @@
 import { ReactNode } from "react";
+import Sidebar from "@/components/Sidebar";
 
 export default function HomeLayout({ children }: { children: ReactNode }) {
   return (
     <main className="relative">
       Navbar
       <div className="flex">
-        Sidebar
+        <Sidebar />
         <section className="flex flex-col flex-1 min-h-screen px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
           <div className="w-full">{children}</div>
         </section>
