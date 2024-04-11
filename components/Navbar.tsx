5c8adab2ed61af85e01609dafeb722f04005a34c
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-11 17:55:42 +0330

    Implementing Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,26 @@
+import Link from "next/link";
+import Image from "next/image";
+import MobileNav from "@/components/MobileNav";
+
 export default function Navbar() {
-  return <h2>Navbar</h2>;
+  return (
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          alt="LetsMeet Logo"
+          height={32}
+          width={32}
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] text-white font-extrabold max-sm:hidden">
+          LetsMeet
+        </p>
+      </Link>
+
+      <div className="flex-between gap-5">
+        <MobileNav />
+      </div>
+    </nav>
+  );
 }
