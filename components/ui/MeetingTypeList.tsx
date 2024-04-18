af7ce7e99413c45ba0ba8c6eaa8db87ee2e6c778
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-18 22:34:17 +0330

    Implementing MeetingTypeList component

---
+++ components/ui/MeetingTypeList.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import HomeCard from "@/components/ui/HomeCard";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function MeetingTypeList() {
+  const [_, setMeetingState] = useState<
+    "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
+  >();
+  const router = useRouter();
+  return (
+    <section className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4">
+      <HomeCard
+        img="/icons/add-meeting.svg"
+        title="New Meeting"
+        description="Start an instant meeting"
+        handleClick={() => setMeetingState("isInstantMeeting")}
+        additionalClasses="bg-orange-1"
+      />
+      <HomeCard
+        img="/icons/schedule.svg"
+        title="Schedule Meeting"
+        description="Plan your meeting"
+        handleClick={() => setMeetingState("isScheduleMeeting")}
+        additionalClasses="bg-blue-1"
+      />
+      <HomeCard
+        img="/icons/recordings.svg"
+        title="View Recordings"
+        description="Check out your recordings"
+        handleClick={() => router.push("/recordings")}
+        additionalClasses="bg-purple-1"
+      />
+      <HomeCard
+        img="/icons/join-meeting.svg"
+        title="Join Meeting"
+        description="Via invitation link"
+        handleClick={() => setMeetingState("isJoiningMeeting")}
+        additionalClasses="bg-yellow-1"
+      />
+    </section>
+  );
+}
