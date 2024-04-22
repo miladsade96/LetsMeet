060961f79efbe85d2b6af399c06334e644200f8a
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-22 20:50:06 +0330

    Adding StreamClientProvider to root layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,10 @@
 import { ReactNode } from "react";
+import StreamClientProvider from "@/providers/StreamClientProvider";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  return <main>{children}</main>;
+  return (
+    <main>
+      <StreamClientProvider> {children}</StreamClientProvider>
+    </main>
+  );
 }
