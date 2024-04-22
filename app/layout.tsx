41ae78ca0daff1d7a63382cc4ac52198c9bed527
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-22 20:58:43 +0330

    Adding speed insights feature of vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/toaster";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,6 +33,7 @@ export default function RootLayout({
       >
         <body className={`${inter.className} bg-dark-2`}>
           {children}
+          <SpeedInsights />
           <Toaster />
         </body>
       </ClerkProvider>
