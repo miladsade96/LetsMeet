93b58a407ac4604595bf497436f549ad3b5067c3
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-27 19:26:46 +0330

    Implementing Meeting component

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,3 +1,35 @@
-export default function Meeting({ params }: { params: { id: string } }) {
-  return <h1>Meeting Room: #{params.id}</h1>;
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import { useState } from "react";
+import MeetingSetup from "@/components/MeetingSetup";
+import MeetingRoom from "@/components/MeetingRoom";
+import { useGetCallById } from "@/hooks/useGetCallById";
+import Loader from "@/components/Loader";
+
+export default function Meeting({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const { user, isLoaded } = useUser();
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+  const { call, isCallLoading } = useGetCallById(id);
+
+  if (!isLoaded || isCallLoading) return <Loader />;
+
+  return (
+    <main className="h-screen w-full">
+      <StreamCall call={call}>
+        <StreamTheme>
+          {!isSetupComplete ? (
+            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+          ) : (
+            <MeetingRoom />
+          )}
+        </StreamTheme>
+      </StreamCall>
+    </main>
+  );
 }
