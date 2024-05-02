05c08d4d8f6bc4503f06954e01534a449ab0d77e
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-05-02 18:43:09 +0330

    Defining CallLayout inner component

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -12,7 +12,18 @@ import { cn } from "@/lib/utils";
 type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 
 export default function MeetingRoom() {
-  return <div>Meeting Room</div>;
   const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
   const [showParticipants, setShowParticipants] = useState(false);
+
+  function CallLayout() {
+    switch (layout) {
+      case "grid":
+        return <PaginatedGridLayout />;
+      case "speaker-right":
+        return <SpeakerLayout participantsBarPosition="left" />;
+      default:
+        return <SpeakerLayout participantsBarPosition="right" />;
+    }
+  }
+
 }
