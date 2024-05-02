89183d3637bcf6b0c358c4910863ea191929a664
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-05-02 18:43:41 +0330

    Implementing MeetingRoom component layout

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -26,4 +26,24 @@ export default function MeetingRoom() {
     }
   }
 
+  return (
+    <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
+      <div className="relative flex size-full items-center justify-center">
+        <div className="flex size-full max-w-[1000px] items-center">
+          <CallLayout />
+        </div>
+        <div
+          className={cn("h-[calc(100vh-86px)] hidden ml-2", {
+            "show-block": showParticipants,
+          })}
+        >
+          <CallParticipantsList onClose={() => setShowParticipants(false)} />
+        </div>
+      </div>
+
+      <div className="fixed bottom-0 w-full flex items-center justify-center gap-5">
+        <CallControls />
+      </div>
+    </section>
+  );
 }
