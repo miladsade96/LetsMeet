135d8c15d2a86e5fbf197b6a1c573c420dda1c01
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-05-02 18:41:18 +0330

    Defining required states for component

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 export default function MeetingRoom() {
   return <div>Meeting Room</div>;
+  const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
+  const [showParticipants, setShowParticipants] = useState(false);
 }
