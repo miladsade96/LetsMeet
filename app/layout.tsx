c09d047d5ee99b1b8c22ab17af748015864020e8
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-05-02 18:39:27 +0330

    Importing main stream io styles to display its components properly

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/toaster";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import "@stream-io/video-react-sdk/dist/css/styles.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
