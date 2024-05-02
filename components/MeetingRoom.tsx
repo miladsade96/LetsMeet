adc53351ef8de6e0e315ca27cb6ac310ca118792
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-05-02 18:42:11 +0330

    Importing relevant modules

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,7 +1,16 @@
 "use client";
 
 import { useState } from "react";
+import {
+  CallControls,
+  CallParticipantsList,
+  PaginatedGridLayout,
+  SpeakerLayout,
+} from "@stream-io/video-react-sdk";
+import { cn } from "@/lib/utils";
+
 type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
+
 export default function MeetingRoom() {
   return <div>Meeting Room</div>;
   const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
