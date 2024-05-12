01edb040543a861deae118bebab42aa0fca5c33e
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2024-05-13 00:20:35 +0330

    Show call participants

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -18,7 +18,11 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { LayoutList, Users } from "lucide-react";
+import { Button } from "@/components/ui/button";
 import { useSearchParams } from "next/navigation";
+import EndCallButton from "@/components/EndCallButton";
+
 type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 
 export default function MeetingRoom() {
@@ -80,6 +84,11 @@ export default function MeetingRoom() {
           </DropdownMenuContent>
         </DropdownMenu>
         <CallStatsButton />
+        <Button onClick={() => setShowParticipants((prevState) => !prevState)}>
+          <div className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
+            <Users size={20} className="text-white" />
+          </div>
+        </Button>
         {!isPersonalRoom && <EndCallButton />}
       </div>
     </section>
