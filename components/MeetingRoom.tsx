21e13661d40509862e69c1fdff8a0e42558cb3d5
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2024-05-12 23:25:05 +0330

    Using drop-down menu to change call layout

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -51,6 +51,29 @@ export default function MeetingRoom() {
 
       <div className="fixed bottom-0 w-full flex items-center justify-center gap-5">
         <CallControls />
+
+        <DropdownMenu>
+          <div className="flex items-center">
+            <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
+              <LayoutList size={20} className="text-white" />
+            </DropdownMenuTrigger>
+          </div>
+          <DropdownMenuContent className="border-dark-1 bg-dark-1 text-white">
+            {["Grid", "Speaker-Left", "Speaker-Right"].map((item, index) => (
+              <div key={index}>
+                <DropdownMenuItem
+                  className="cursor-pointer"
+                  onClick={() =>
+                    setLayout(item.toLowerCase() as CallLayoutType)
+                  }
+                >
+                  {item}
+                </DropdownMenuItem>
+                <DropdownMenuSeparator className="border-dark-1" />
+              </div>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </section>
   );
