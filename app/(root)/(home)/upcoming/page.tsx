e40613ca9a3b4e3a7d9cedc7439b029d24e71f4a
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2024-05-14 16:35:00 +0330

    Adding CallList component to upcoming page

--- app/(root)/(home)/upcoming/page.tsx
+++ app/(root)/(home)/upcoming/page.tsx
@@ -1,7 +1,10 @@
-export default function page() {
+import CallList from "@/components/CallList";
+
+export default function Upcoming() {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
       <h1 className="text-3xl font-bold">Upcoming</h1>
+      <CallList type="upcoming" />
     </section>
   );
 }
